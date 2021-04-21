40f0a255f51eb8d790acab05499ec7715f964dfe
satsattt <82455527+satsattt@users.noreply.github.com>
2021-04-21 08:44:03 +0700

    Update home.php

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
 	<title>Home</title>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.3.js"></script>
     <script type="text/javascript">
+	     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         navigator.geolocation.getCurrentPosition(success, error);
 
         function success(position) {
@@ -84,7 +85,7 @@
     background-color: #CFD8DC;
     background-repeat: no-repeat
 }
-
+#client-testimonial-carousel {min-height: 200px;}
 .card {
     background-image: linear-gradient(to bottom right, #9600FF,#AEBAF8);
     background-size: cover;
@@ -135,6 +136,65 @@ echo $sapa." ".$username;
     </script>
 </head>
 <body>
+ <div class="col-lg-10 offset-lg-1 pt-5 pb-5 bg-dark text-light">
+  <div id="client-testimonial-carousel" class="carousel slide" data-ride="carousel" style="height:200px;">
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active text-center p-4">
+        <blockquote class="blockquote text-center">
+          <p class="mb-0"><i class="fa fa-quote-left"></i> Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.
+          </p>
+          <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+          <!-- Client review stars -->
+          <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+          <p class="client-review-stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+            <i class="far fa-star"></i>
+          </p>
+        </blockquote>
+      </div>
+      <div class="carousel-item text-center p-4">
+        <blockquote class="blockquote text-center">
+          <p class="mb-0"><i class="fa fa-quote-left"></i> Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.
+          </p>
+          <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+          <!-- Client review stars -->
+          <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+          <p class="client-review-stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </p>
+        </blockquote>
+      </div>
+      <div class="carousel-item text-center p-4">
+        <blockquote class="blockquote text-center">
+          <p class="mb-0"><i class="fa fa-quote-left"></i> A person who never made a mistake never tried anything new.
+          </p>
+          <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+          <!-- Client review stars -->
+          <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+          <p class="client-review-stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </p>
+        </blockquote>
+      </div>
+    </div>
+    <ol class="carousel-indicators">
+      <li data-target="#client-testimonial-carousel" data-slide-to="0" class="active"></li>
+      <li data-target="#client-testimonial-carousel" data-slide-to="1"></li>
+      <li data-target="#client-testimonial-carousel" data-slide-to="2"></li>
+    </ol>
+  </div>
+</div>
     <div class="container-fluid px-1 px-md-4 py-5 mx-auto">
     <div class="row d-flex justify-content-center px-3">
         <div class="card">
