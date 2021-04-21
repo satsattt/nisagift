dc75686c4fe29e3ccf93b395b25f62f0d4c6eb54
satsattt <82455527+satsattt@users.noreply.github.com>
2021-04-21 21:39:08 +0700

    Update akses.php

--- akses.php
+++ akses.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['user'])){
+if(empty($_SESSION['user'])){
 	echo '<script language="javascript">alert("Anda harus Login!"); document.location="../index.html";</script>';
 }
 ?>
