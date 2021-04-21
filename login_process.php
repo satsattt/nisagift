5addd8dcb5a3d0bd0f0b28027a8c0c162d10c707
satsattt <82455527+satsattt@users.noreply.github.com>
2021-04-21 08:25:04 +0700

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -5,6 +5,6 @@ if(($username=="nisa"||$username=="satsat")&&$password=="2104"){
 $_SESSION['user']=$username;
 			echo '<script language="javascript">alert("Anda berhasil Login!"); document.location="../home.php";</script>';
 }else{
-     	echo '<script language="javascript">alert("Login anda gagal!")'; 
+     	echo '<script language="javascript">alert("Login anda gagal!")</script>'; 
 }
 ?>
