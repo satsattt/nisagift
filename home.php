2ee3b7abfb41fcf8ed6257da04f77dee1a41706b
satsattt <82455527+satsattt@users.noreply.github.com>
2021-04-21 19:14:51 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,86 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
-	<script src=”https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js”></script>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-	<meta charset="utf-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<style>
-	body { 
-	background-color: rgb(30, 15, 33);
-	margin: 0;
-	padding: 0;
-}
-#client-testimonial-carousel {min-height: 200px;}
-	</style>
-	<script src="https://unpkg.com/@popperjs/core@2"></script>
-	   <script>
-      const button = document.querySelector('#button');
-      const tooltip = document.querySelector('#tooltip');
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      const popperInstance = Popper.createPopper(button, tooltip);
-    </script>
-</head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-<div class="col-lg-10 offset-lg-1 pt-5 pb-5 bg-dark text-light">
-  <div id="client-testimonial-carousel" class="carousel slide" data-ride="carousel" style="height:200px;">
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active text-center p-4">
-        <blockquote class="blockquote text-center">
-          <p class="mb-0"><i class="fa fa-quote-left"></i> Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.
-          </p>
-          <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
-          <!-- Client review stars -->
-          <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
-          <p class="client-review-stars">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star-half-alt"></i>
-            <i class="far fa-star"></i>
-          </p>
-        </blockquote>
-      </div>
-      <div class="carousel-item text-center p-4">
-        <blockquote class="blockquote text-center">
-          <p class="mb-0"><i class="fa fa-quote-left"></i> Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.
-          </p>
-          <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
-          <!-- Client review stars -->
-          <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
-          <p class="client-review-stars">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </p>
-        </blockquote>
-      </div>
-      <div class="carousel-item text-center p-4">
-        <blockquote class="blockquote text-center">
-          <p class="mb-0"><i class="fa fa-quote-left"></i> A person who never made a mistake never tried anything new.
-          </p>
-          <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
-          <!-- Client review stars -->
-          <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
-          <p class="client-review-stars">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </p>
-        </blockquote>
-      </div>
-    </div>
-    <ol class="carousel-indicators">
-      <li data-target="#client-testimonial-carousel" data-slide-to="0" class="active"></li>
-      <li data-target="#client-testimonial-carousel" data-slide-to="1"></li>
-      <li data-target="#client-testimonial-carousel" data-slide-to="2"></li>
-    </ol>
+    <title>Home</title>
+  </head>
+  <body>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+   <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../kittyLitter.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Home
+    </a>
   </div>
-</div>
+</nav>
+
+</body>
 </html>
