78373255a18f3b9d7d9aa59c5b486c1af0c51f48
satsattt <82455527+satsattt@users.noreply.github.com>
2021-04-21 09:07:14 +0700

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<script src=”https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js”></script>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.2/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -15,6 +15,12 @@
 #client-testimonial-carousel {min-height: 200px;}
 	</style>
 	<script src="https://unpkg.com/@popperjs/core@2"></script>
+	   <script>
+      const button = document.querySelector('#button');
+      const tooltip = document.querySelector('#tooltip');
+
+      const popperInstance = Popper.createPopper(button, tooltip);
+    </script>
 </head>
 
 <div class="col-lg-10 offset-lg-1 pt-5 pb-5 bg-dark text-light">
